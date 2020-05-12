918ac8582ed1b3a70903f12594cc585f6cbff31d
cellista04 <65170236+cellista04@users.noreply.github.com>
2020-05-12 16:54:40 +0200

    Update funchata.js

--- funchata.js
+++ funchata.js
@@ -1,1 +1,16 @@
-
+var inicio=document.getElementById("inicio");
+var chat=document.getElementById("chat");
+var mensajes=document.getElementById("mensajes");
+var email;
+var name;
+var password;
+var user;
+var socket;
+var admin=false;
+function crear(){
+  name=document.getElementById("name_C").value;
+  email=document.getElementById("email_c").value;
+  user=document.getElementById("user_c").value;
+  password=document.getElementById("clave_c").value;
+  admin=true;
+}
