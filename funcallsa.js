8daf08d21ace21763369a52112ba632c39c4a2a9
cellista04 <65170236+cellista04@users.noreply.github.com>
2020-05-12 12:01:41 +0200

    Add files via upload

---
+++ funcallsa.js
@@ -0,0 +1,14 @@
+var reg=document.getElementById("registro");
+var call=document.getElementById("call");
+function leer(){
+	var url=document.location.href;
+	if (url.indexOf("?")){
+		var clave=url.split("?")[1].split("=")[0];
+		reg.style.display="none";
+		call.style.display="block";
+	}
+}
+leer();
+function finalizar_call(){
+	location.href="https://cellista04.github.io";
+}
